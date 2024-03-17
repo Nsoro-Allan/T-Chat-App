150903d5aa4076ca66783ca6f89316415bda0b51
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-03-17 19:28:15 +0200

    changes made...

--- index.php
+++ index.php
@@ -23,7 +23,15 @@
                     <img src="./imgs/img1.png" alt="Avatar">
                     <h4>Sent by <span>@Nsoro Allan</span></h4>
                 </div>
-
+                <div class="card-content">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque praesentium, ea quisquam optio maxime sed minus facilis eius recusandae aliquid at accusamus possimus cum necessitatibus, incidunt perferendis consequuntur? Minus, voluptatem?</p>
+                </div>
+                <div class="card-end">
+                    <button>👍 0</button>
+                    <button>😄 0</button>
+                    <button>🔥 0</button>
+                    <button>👎 0</button>
+                </div>
             </div>
         </div>
         <div class="end">
