57915d9bea95c742b525a105eb6c8a74b7a1b158
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-03-17 18:36:56 +0200

    some changes added...

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>T - Chat</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="./imgs/icon.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="main-container">
+        <div class="title">
+            <a href="./index.php"><h1><img src="./imgs/icon.ico" alt=""> T - Chat</h1></a>
+            <div class="line"></div>
+        </div>
+        <div class="content">
+        </div>
+        <div class="end">
+            <form action="" method="post">
+                <input type="text" name="message" placeholder="Type your message here...">
+                <button type="submit" name="send_message"><img src="./imgs/send.ico" alt=""></button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
