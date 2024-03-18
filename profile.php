6246cf78db0d4c5ba573aa76898c1e864ec2642b
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-03-18 16:43:54 +0200

    changes made...

---
+++ profile.php
@@ -0,0 +1,30 @@
+<?php
+include("connection.php");
+include("sessions.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>T - Chat || Profile</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="./imgs/icon.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="main-container">
+        <div class="title">
+            <a href="./chat"><h1><img src="./imgs/icon.ico" alt=""> T - Chat - [Profile]</h1></a>
+            <div class="line"></div>
+        </div>
+        <div class="details">
+            <h4>Logged In As @<a href="#"><?php echo $_SESSION['chat_username'];?></a></h4>
+            <a href="./logout.php" class="btn">SignOut</a>
+        </div>
+        <div class="content">
+            .account
+        </div>
+    </div>
+</body>
+</html>
